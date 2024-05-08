4c67d7fd7af7b3d18140b403d6cc05224bab6066
etoki <sub.ashuman@gmail.com>
2024-05-08 10:51:19 +0900

    modified:   sql/cnt.sql

--- sql/cnt.sql
+++ sql/cnt.sql
@@ -1,1 +1,13 @@
-select count(*), count(distinct userId) from response;
+select 
+    count(*) as ans
+    , count(distinct r.userId) as u_ans
+    , avg((r.endTimestamp - r.startTimestamp)/60) as avg_diff
+    , sum(r.completed) as comp
+    , sum(r.completed)/count(*) as comp_rate
+    , count(DISTINCT u.email) as uu
+    , count(DISTINCT u.email)/count(distinct r.userId) as auth_rate
+from response r
+left JOIN
+user u
+ON r.userId = u.id
+;
